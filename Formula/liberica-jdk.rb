8b45469ee0abdfe06ab57482a6eeede736aad895
TANIGUCHI Masaya <xtaniguchimasaya+code@gmail.com>
2019-05-15 13:31:14 +0900

    , -> .

--- Formula/liberica-jdk.rb
+++ Formula/liberica-jdk.rb
@@ -2,7 +2,7 @@ class LibericaJdk < Formula
   desc "100% open-source Java implementation"
   homepage "https://bell-sw.com/"
   url "https://download.bell-sw.com/java/8u212/bellsoft-jdk8u212-linux-amd64.tar.gz"
-  version "1.8.0,212"
+  version "1.8.0.212"
   sha256 "4ca9d04728e4d03a3467f269b112ad54d31a5cc7008946ea525abe75b86c4946"
 
   depends_on :linux
