f50aba68080fb325c4f7aaa117da31db1c9dea20
Kirill Gavrilov <gavvvr@ya.ru>
2019-10-08 21:22:33 +0300

    Fix checksum

--- Casks/liberica-jdk11.rb
+++ Casks/liberica-jdk11.rb
@@ -1,6 +1,6 @@
 cask 'liberica-jdk11' do
   version '11.0.4,10'
-  sha256 'a99b1d9e8378b585fad2fe97b7aeff16053d93c814c909d6fa7d3749948ec91a'
+  sha256 '5450d6f929bdc700222bd8866b96abe3defd39c8babd084e6f59202818386ff0'
 
   url "https://download.bell-sw.com/java/11.0.4/bellsoft-jdk11.0.4-macos-amd64.pkg"
   name 'BellSoft Liberica JDK 11'
