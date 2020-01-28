00a0d8721001b2912bc1c1092712abd5e6e9570a
Dima Strizhikozin <dmitry.strizhikozin@bell-sw.com>
2020-01-28 14:01:10 +0300

    fix sha256 for jdk8full

--- Casks/liberica-jdk8-full.rb
+++ Casks/liberica-jdk8-full.rb
@@ -1,6 +1,6 @@
 cask 'liberica-jdk8-full' do
   version '1.8.0,242'
-  sha256 '2037e03ad361a8ff36e8edd8ca1f2d9ec8d8572b41a2e53f06bf0291b1b972d7'
+  sha256 '771f3671d7b26c2ecda1287f5cb9fb9209e72185a6c20a7ac1a94798e1c2c15d'
 
   url "https://download.bell-sw.com/java/8u242%2B7/bellsoft-jdk8u242%2B7-macos-amd64-full.pkg"
   name 'BellSoft Liberica JDK 8'
