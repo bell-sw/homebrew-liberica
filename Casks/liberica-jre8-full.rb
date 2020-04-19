f144dcf06d222b9eba1e03e00a6e412c5cc912c6
Denis Kononenko <denis.kononenko@bell-sw.com>
2020-04-19 11:56:51 +0300

    Fixed checksums errors caused by CDN cache issues.

--- Casks/liberica-jre8-full.rb
+++ Casks/liberica-jre8-full.rb
@@ -1,6 +1,6 @@
 cask 'liberica-jre8-full' do
   version '1.8.0,252'
-  sha256 'e31510c92f644ff0f9ee414fe2f0d24d84674b3df4409ab4478f6b089c0fbb74'
+  sha256 '424ac6429525e2f9f8e2bfd06052d98a273bc89fcc8ea10f56eda6f1071ca8ad'
 
   url "https://download.bell-sw.com/java/8u252%2B9/bellsoft-jre8u252%2B9-macos-amd64-full.pkg"
   name 'BellSoft Liberica JRE 8'
--- Formula/liberica-jdk.rb
+++ Formula/liberica-jdk.rb
@@ -3,7 +3,7 @@ class LibericaJdk < Formula
   homepage "https://bell-sw.com/"
   url "https://download.bell-sw.com/java/8u252%2B9/bellsoft-jdk8u252%2B9-linux-amd64.tar.gz"
   version "1.8.0.252"
-  sha256 "0654d01fbf03e0bf34606d416fccf05552a8a71c9776535b3dad583c9fe654cc"
+  sha256 "cbf38656ae0e0f8dc47e810430498d85f08331e8c84aafc3aa1cd2ceb83cf409"
 
   depends_on :linux
 
